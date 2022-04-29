c1cfa6a1309ed85f9442f05e458356401e3a389f
jamesthesken <jthesken@hawaii.edu>
2022-04-29 09:04:26 -1000

    add prose-invert to fix dark mode on posts

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -8,7 +8,7 @@ export default function Post({ data, content }) {
       <div>
         <h1 className="font-bold text-7xl mt-24 mb-12">{data.title}</h1>
         <time className="text-gray-500 italic">{data.date}</time>
-        <p className=" prose mt-12">
+        <p className="prose dark:prose-invert mt-12">
           <MDXRemote {...content} />
         </p>
       </div>
