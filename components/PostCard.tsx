9f1b79235a9dc4315e9a957e6d30aaaab75af965
jamesthesken <jthesken@hawaii.edu>
2022-04-29 08:50:19 -1000

    Clean up post and index

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link"
+
 export default function PostCard(props) {
     return (
         <div>
-            <h2>{props.title}</h2>
-            <p>{props.date}</p>
+            <h2 className="mb-4 font-bold text-2xl">{props.title}</h2>
+            <p className="mb-4 text-gray-500 text-sm">{props.date}</p>
+            <p className="mb-4 text-sm italic">{props.description}</p>
+            <Link href={`/posts/${props.slug}`}>
+                <a className="text-indigo-800 text-sm">Read more</a>
+            </Link>
+            <div className="mt-4 border-t border-gray-300"></div>
         </div>
     )
 }
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,38 @@
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import { getPosts, getPost } from "../../helpers/getPosts";
+
+
+export default function Post({ data, content }) {
+    return (
+      <div>
+        <h1 className="font-bold text-7xl mt-24 mb-12">{data.title}</h1>
+        <time className="text-gray-500 italic">{data.date}</time>
+        <p className=" prose mt-12">
+          <MDXRemote {...content} />
+        </p>
+      </div>
+    );
+};
+
+
+export const getStaticPaths = async () => {
+    const posts = await getPosts();
+    const paths = posts['allPostsData'].map((post) => ({ params: { slug: post.slug } }));
+    return {
+      paths,
+      fallback: false,
+    };
+  };
+
+export const getStaticProps = async ({ params }) => {
+    const post = await getPost(params.slug);
+    const mdxSource = await serialize(post.content);
+    return {
+        props: {
+        data: post.data,
+        content: mdxSource,
+        },
+    };
+};
+
