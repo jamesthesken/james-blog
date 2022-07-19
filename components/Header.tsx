f5e2d25ced056e6b8575058e21abe4f2a1368678
jamesthesken <jthesken@hawaii.edu>
2022-07-19 10:54:33 -1000

    style: add icon to footer and change menu color

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,7 +16,7 @@ function Header({}) {
             </div>
             <div className="block lg:hidden">
                 <button onClick={() => setIsOpen(!isOpen)} className="flex items-center px-3 py-2 border rounded dark:text-slate-200 border-slate-400 hover:text-white hover:border-white">
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                <svg className="bg-slate-200 fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
             <div className={isOpen ?  "w-full block flex-grow lg:flex lg:items-center lg:w-auto" : "hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto"}>
@@ -26,9 +26,6 @@ function Header({}) {
                     </Link>
                     <Link href={'/about'}>
                         <a className="block mt-4 lg:inline-block lg:mt-0 dark:text-slate-200 hover:text-white mr-4">About</a>
-                    </Link>
-                    <Link href={'/contact'}>    
-                        <a className="block mt-4 lg:inline-block lg:mt-0 dark:text-slate-200 hover:text-white mr-4">Contact</a>
                     </Link>
                     </div>
                     <div className="mt-4">
