3edddc22b02367116bacb03cbac3adf329c45d75
jamesthesken <jthesken@hawaii.edu>
2022-07-19 09:01:41 -1000

    feat: responsive header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,49 +1,44 @@
 import Link from "next/link";
+import { useState } from "react";
 import LightSwitch from "./LightSwitch";
 
 function Header({}) {
-  return (
-    <section>
-        <nav className="flex justify-between items-center py-6 px-10 mt-5 relative">
-        <Link href={'/'}>
-            <a className="text-lg font-bold dark:text-terminal">
-                James Thesken
-            </a>
-        </Link>
-        <div className="xl:hidden">
-            <button className="navbar-burger">
-                <svg className="block h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Mobile menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg>
-            </button>
-        </div>
-        <ul className="hidden xl:flex absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <Link href={'/'}>
-                <li><a className="text-lg mr-10 2xl:mr-16 font-extrabold hover:text-indigo-800">Blog</a></li>
-            </Link>
-            <Link href={'/about'}>
-                <li><a className="text-lg mr-10 2xl:mr-16 font-extrabold hover:text-indigo-800">About</a></li>
-            </Link>
-            <Link href={'/contact'}>    
-                <li><a className="text-lg mr-10 2xl:mr-16 font-extrabold hover:text-indigo-800">Contact</a></li>
-            </Link>
-            <li><LightSwitch /></li>
-        </ul>
-        </nav>
-        <div className="hidden navbar-menu relative z-50">
-            <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-            <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-full md:w-5/6 max-w-sm py-8 px-8 bg-white border-r overflow-y-auto"><div className="flex items-center mb-8">
-                <button className="navbar-close">
-                <svg className="h-6 w-6 text-gray-500 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg></button>
-                </div>
-                <div className="my-auto">
-                <ul className="py-10">
-                    <li className="mb-1"><a className="block p-4 text-lg font-extrabold hover:bg-gray-50 rounded" href="#">Blog</a></li>
-                    <li className="mb-1"><a className="block p-4 text-lg font-extrabold hover:bg-gray-50 rounded" href="#">About</a></li>
-                    <li className="mb-1"><a className="block p-4 text-lg font-extrabold hover:bg-gray-50 rounded" href="#">Contact</a></li>
-                </ul></div>
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <section>
+            <nav className="flex items-center justify-between flex-wrap p-6 px-10">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <Link href={'/'}>
+                    <a className="text-lg font-bold dark:text-terminal">
+                        James Thesken
+                    </a>
+                </Link>
+            </div>
+            <div className="block lg:hidden">
+                <button onClick={() => setIsOpen(!isOpen)} className="flex items-center px-3 py-2 border rounded dark:text-slate-200 border-slate-400 hover:text-white hover:border-white">
+                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+            <div className={isOpen ?  "w-full block flex-grow lg:flex lg:items-center lg:w-auto" : "hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto"}>
+                    <div className="text-sm lg:flex-grow lg:ml-20">
+                    <Link href={'/'}>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 dark:text-slate-200 hover:text-white mr-4">Blog</a>
+                    </Link>
+                    <Link href={'/about'}>
+                        <a className="block mt-4 lg:inline-block lg:mt-0 dark:text-slate-200 hover:text-white mr-4">About</a>
+                    </Link>
+                    <Link href={'/contact'}>    
+                        <a className="block mt-4 lg:inline-block lg:mt-0 dark:text-slate-200 hover:text-white mr-4">Contact</a>
+                    </Link>
+                    </div>
+                    <div className="mt-4">
+                    <LightSwitch />
+                    </div>
+            </div>
             </nav>
-        </div>
-    </section>
-  );
+            
+        </section>
+    );
 }
 
 export default Header;
